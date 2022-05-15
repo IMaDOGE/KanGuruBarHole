2c200264138f00241989b6efc946771469de12f4
david9706 <david970681@gmail.com>
2022-05-15 22:03:09 +0200

    teszt osztályok létrehozása csapó 2

---
+++ src/main/java/hu/unideb/inf/tests/FXML/FXMLAddNewDrinkControllerTest.java
@@ -0,0 +1,35 @@
+package hu.unideb.inf.tests.FXML;
+
+import hu.unideb.inf.entity.User;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+
+public class FXMLAddNewDrinkControllerTest {
+    @Mock
+    private int NemEzKellIde; // csak idedobtam valamit, hogy ne huzza ala az @Mock-ot
+
+    @BeforeAll //@BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterAll //@AfterClass
+    public static void tearDownClass() {
+    }
+
+
+    @BeforeEach //@Before
+    public void setUp() {
+        // ...
+    }
+
+    @AfterEach //@After
+    public void tearDown() {
+        // ...
+    }
+
+    @Test
+    public void testValami(){
+        // ..
+    }
+    // TODO
+}
---
+++ src/main/java/hu/unideb/inf/tests/FXML/FXMLInfoControllerTest.java
@@ -0,0 +1,34 @@
+package hu.unideb.inf.tests.FXML;
+
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+
+public class FXMLInfoControllerTest {
+    @Mock
+    private int NemEzKellIde; // csak idedobtam valamit, hogy ne huzza ala az @Mock-ot
+
+    @BeforeAll //@BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterAll //@AfterClass
+    public static void tearDownClass() {
+    }
+
+
+    @BeforeEach //@Before
+    public void setUp() {
+        // ...
+    }
+
+    @AfterEach //@After
+    public void tearDown() {
+        // ...
+    }
+
+    @Test
+    public void testValami(){
+        // ..
+    }
+    // TODO
+}
