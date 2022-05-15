a06ccbcf3e0217cd2a0d51a0f1f83361fe77d5dd
Zoltán Tóth <zolcsika2001@gmail.com>
2022-05-15 20:30:02 +0200

    Unit test dependency-k hozzáadva a pom.xml-be. Teszt osztályok létrehozása.

---
+++ src/main/java/hu/unideb/inf/tests/Entity/FinanceTest.java
@@ -0,0 +1,35 @@
+package hu.unideb.inf.tests.Entity;
+
+import hu.unideb.inf.entity.Finance;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+
+public class FinanceTest {
+    @Mock
+    private Finance underTest1, underTest2;
+
+    @BeforeAll //@BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterAll //@AfterClass
+    public static void tearDownClass() {
+    }
+
+
+    @BeforeEach //@Before
+    public void setUp() {
+        // ...
+    }
+
+    @AfterEach //@After
+    public void tearDown() {
+        // ...
+    }
+
+    @Test
+    public void testValami(){
+        // ..
+    }
+    // TODO
+}
